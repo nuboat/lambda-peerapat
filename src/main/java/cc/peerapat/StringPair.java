5697bdfd1bfebbe5ba6c6efb107bb0ab703a913e
Peerapat A <i@peerapat.cc>
2022-09-25 21:42:11 +0700

    initial project

---
+++ src/main/java/cc/peerapat/StringPair.java
@@ -0,0 +1,17 @@
+package cc.peerapat;
+
+/**
+ *  @author Peerapat A, Sep 26, 2017
+ */
+public final class StringPair {
+
+    public final String before;
+
+    public final String after;
+
+    public StringPair(final String before, final String after) {
+        this.before = before;
+        this.after = after;
+    }
+
+}
